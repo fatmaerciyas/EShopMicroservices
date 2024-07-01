5f6e5591d4432de34dab9fd8db16253110be2b83
fatmaerciyas <fatmaerciyas22@gmail.com>
2024-07-01 11:35:42 +0300

    added models

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Category { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string Image { get; set; } = default!;
+    public decimal Price { get; set; };
+}
